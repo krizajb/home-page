ac257a046481a064e1142c947b100f930b45feb4
Blaž Križaj <blaz.krizaj@gmail.com>
2018-03-16 19:56:34 +0100

    Moved to custom responsive handling. Draft finished, still rough on the edges.

---
+++ assets/js/qord.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  // var currentWidth = 0;
+  // $(window).resize(function() {
+  //   //console.log($(window).width())
+  //
+  //   if ($(window).width() < 410) {
+  //     $('.o').each(function() {
+  //       var width = parseInt($(this).css('width').replace('px',''));
+  //       var newWidth = $(window).width() * 0.217;
+  //       console.log(width+" "+newWidth)
+  //       $(this).css('width', newWidth);
+  //       var ratio = newWidth/width;
+  //       console.log(ratio)
+  //       $(this).css('height', $('.o').css('height') * ratio);
+  //     });
+  //   }
+  //   else if ($(window).width() <= 510) {
+  //     $('.o').each(function() {
+  //       $(this).css('width', 89);
+  //       $(this).css('height', 168);
+  //     });
+  //   }
+  //   else {
+  //     $('.o').each(function() {
+  //       $(this).css('width', 112);
+  //       $(this).css('height', 210);
+  //     });
+  //   }
+  // });
+
+
+});
